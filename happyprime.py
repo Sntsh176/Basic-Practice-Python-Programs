9509dc763556da524a3b67fad9146f7dcf8546aa
Sntsh176 <santurock177@gmail.com>
2019-02-05 21:55:19 +0530

    Create happyprime.py
    happy prime numbers

---
+++ happyprime.py
@@ -0,0 +1,20 @@
+
+# !/use/bin/env python3
+# https://www.practicepython.org/exercise/2014/12/14/23-file-overlap.html
+
+if __name__ == "__main__":
+	with open('happy.txt', 'r') as open_file:
+		text=open_file.read()
+		
+	with open('prime.txt', 'r') as open_file:
+		text2=open_file.read()
+	
+	list_happy = map(int,text.split(","))
+	list_prime = map(int,text2.split(","))
+	
+	print "Happy Numbers:" 
+	print list_happy
+	print "\nPrime Numbers:"
+	print list_prime
+	print "\nOverlap:"
+	print sorted(list(set(list_happy)&set(list_prime)))
